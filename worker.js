e7686608b2f4bc7180a237237c7cf87d388c5855
sewon92 <parksw206@naver.com>
2026-06-03 10:55:33 +0900

    Add files via upload

---
+++ worker.js
@@ -0,0 +1,11 @@
+const CACHE_NAME = 'purine-guide-v2';
+self.addEventListener('install', function(e){ self.skipWaiting(); });
+self.addEventListener('activate', function(e){
+  e.waitUntil(caches.keys().then(function(keys){
+    return Promise.all(keys.map(function(k){ return caches.delete(k); }));
+  }));
+  self.clients.claim();
+});
+self.addEventListener('fetch', function(e){
+  e.respondWith(fetch(e.request).catch(function(){ return caches.match(e.request); }));
+});
